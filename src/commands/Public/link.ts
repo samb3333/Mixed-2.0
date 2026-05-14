1a937bc131e74c8a6b37cbcb77d643b28bb59d24
samb3333 <samuel.barlow333@gmail.com>
2026-05-14 21:59:36 +0100

    changed edit_name to link

--- src/commands/Public/link.ts
+++ src/commands/Public/link.ts
@@ -4,20 +4,20 @@ import { PlayerManager } from '../../classes/PlayerManager';
 const players = PlayerManager.getInstance();
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('edit_name').setDescription('Edit your name').addStringOption(option =>
-      option.setName('name').setDescription('The name of the tournament').setRequired(true)
+	data: new SlashCommandBuilder().setName('link').setDescription('Edit your name').addStringOption(option =>
+      option.setName('meta').setDescription('Your meta name').setRequired(true)
     ),
 
 	async execute(interaction: ChatInputCommandInteraction) {
 		await interaction.deferReply({ ephemeral: true });
-        const name = interaction.options.getString('name', true);
+        const name = interaction.options.getString('meta', true);
 		const player = players.editUsername(interaction.user.id, name);
 		if (player === 'not_found') {
-			await interaction.editReply('You are not registered!');
+			players.register(interaction.user.id, name);
+			await interaction.editReply('You are now registered!');
 			return;
 		}
 		await interaction.editReply('Name edited!');
-
 
 	}
 };
